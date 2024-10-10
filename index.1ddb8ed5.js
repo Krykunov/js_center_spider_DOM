413e872961931d057ef4de7571ecd775ff4c3c80
Igor <4430704@gmail.com>
2024-10-10 10:47:40 +0300

    make build

---
+++ index.1ddb8ed5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.top="".concat((c-i)/2,"px"),t.style.left="".concat((l-n)/2,"px");
+//# sourceMappingURL=index.1ddb8ed5.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const field = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const fieldWidth = field.clientWidth;
+const fieldHeight = field.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const spiderLeft = (fieldWidth - spiderWidth) / 2;
+const spiderTop = (fieldHeight - spiderHeight) / 2;
+spider.style.top = `${spiderLeft}px`;
+spider.style.left = `${spiderTop}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
